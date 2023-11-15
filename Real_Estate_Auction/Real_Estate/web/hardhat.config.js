b3e117c2c40ba1ebd66b031988061d6e1493c6a1
Padigela Vaidika <120257066+vaidikapadigela@users.noreply.github.com>
2023-11-15 23:53:49 +0530

    Added code for deployment into Mumbai Polygon

--- Real_Estate_Auction/Real_Estate/web/hardhat.config.js
+++ Real_Estate_Auction/Real_Estate/web/hardhat.config.js
@@ -1,7 +1,20 @@
 require("@matterlabs/hardhat-zksync-solc");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const PRIVATE_KEY= "54d0eefe352ed6b23ff78ccbbd54deaf2bc9f2772ea8e840f257d862923e0fd2";
+const RPC_URL="https://polygon-mumbai.g.alchemy.com/v2/tLq8XLigUM_0b1zokcCpakKmMfjuvCil"
 module.exports = {
+  defaultNetwork:"MATIC_MUMBAI",
+  networks:{
+    hardhat:{
+      chainId:80001
+    },
+  MATIC_MUMBAI:{
+    url:"https://polygon-mumbai.g.alchemy.com/v2/7ok0ERmj9jzuRO0Sv3B8xJLOo_oeAPIA",
+    accounts:[`0x${PRIVATE_KEY}`]
+  }
+},
   zksolc: {
     version: "1.3.9",
     compilerSource: "binary",
@@ -24,6 +37,16 @@ module.exports = {
       chainId: 324,
       zksync: true,
     },
+  
+      hardhat:{
+        chainId:80001
+      },
+    MATIC_MUMBAI:{
+      url:"https://polygon-mumbai.g.alchemy.com/v2/7ok0ERmj9jzuRO0Sv3B8xJLOo_oeAPIA",
+      accounts:[`0x${PRIVATE_KEY}`]
+    },
+    
+
   },
   paths: {
     artifacts: "./artifacts-zk",
