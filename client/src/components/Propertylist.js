17f331b799fcb753f3d73044e82d56749ce87bed
kavyaperepu <perepukavyasri@gmail.com>
2023-12-20 01:03:30 +0530

    added like button

--- client/src/components/Propertylist.js
+++ client/src/components/Propertylist.js
@@ -1,22 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import data from '../data.json';
-import './Propertylist.css'; // Import your CSS file
-
+import './Propertylist.css'; 
+import { AiFillHeart } from 'react-icons/ai';
 
 const Propertylist = () => {
+  const [likedProperties, setLikedProperties] = useState([]);
+
+  const handleLike = (propertyId) => {
+    setLikedProperties((prevLikedProperties) => {
+      if (prevLikedProperties.includes(propertyId)) {
+        // If property is already liked, remove it from the liked properties
+        return prevLikedProperties.filter((id) => id !== propertyId);
+      } else {
+        // If property is not liked, add it to the liked properties
+        return [...prevLikedProperties, propertyId];
+      }
+    });
+  };
+
   return (
-    
     <div className="properties-container">
-      <head>Properties Page</head>
-      
       {data.map((property) => (
-        <div key={property.id} className="property-item">
+        <div key={property._id} className="property-item">
+          <AiFillHeart
+            size={24}
+            color={likedProperties.includes(property._id) ? '#fa3e5f' : 'white'}
+            onClick={() => handleLike(property._id)}
+            className='like-container'
+          />
           <img src={property.image} alt={property.name} className="img" />
-        
-          <p className="attributes">{property.title} <br></br>
-           {property.price} MATIC <br></br>
-          {property.address}, {property.city}, {property.country}</p>
-          <button className="viewButton" >view property </button>
+          <p className="attributes">
+            {property.title}
+            <br />
+            {property.price} MATIC
+            <br />
+            {property.address}, {property.city}, {property.country}
+          </p>
+          <button className="viewButton">View Property</button>
         </div>
       ))}
     </div>
