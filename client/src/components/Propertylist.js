cffc82f4267eb0fa0eb18f40ece08f8c16e57b5e
kavyaperepu <perepukavyasri@gmail.com>
2023-12-19 22:10:31 +0530

    added styling to Buy_property

--- client/src/components/Propertylist.js
+++ client/src/components/Propertylist.js
@@ -5,15 +5,17 @@ import './Propertylist.css'; // Import your CSS file
 
 const Propertylist = () => {
   return (
+    
     <div className="properties-container">
       <head>Properties Page</head>
+      
       {data.map((property) => (
         <div key={property.id} className="property-item">
           <img src={property.image} alt={property.name} className="img" />
         
-          <p>{property.title}</p>
-          <p>{property.price} MATIC</p>
-          <p>{property.address}, {property.city}, {property.country}</p>
+          <p className="attributes">{property.title} <br></br>
+           {property.price} MATIC <br></br>
+          {property.address}, {property.city}, {property.country}</p>
           <button className="viewButton" >view property </button>
         </div>
       ))}
