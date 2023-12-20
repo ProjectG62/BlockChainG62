80c154468745da8662a059b1d675cc369ffe507c
kavyaperepu <perepukavyasri@gmail.com>
2023-12-20 13:29:09 +0530

    added like button

--- client/src/components/Propertylist.js
+++ client/src/components/Propertylist.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import data from '../data.json';
 import './Propertylist.css'; 
+import SearchBar from './SearchBar';
 import { AiFillHeart } from 'react-icons/ai';
 
 const Propertylist = () => {
@@ -19,26 +20,27 @@ const Propertylist = () => {
   };
 
   return (
-    <div className="properties-container">
-      {data.map((property) => (
-        <div key={property._id} className="property-item">
-          <AiFillHeart
-            size={24}
-            color={likedProperties.includes(property._id) ? '#fa3e5f' : 'white'}
-            onClick={() => handleLike(property._id)}
-            className='like-container'
-          />
-          <img src={property.image} alt={property.name} className="img" />
-          <p className="attributes">
-            {property.title}
-            <br />
-            {property.price} MATIC
-            <br />
-            {property.address}, {property.city}, {property.country}
-          </p>
-          <button className="viewButton">View Property</button>
-        </div>
-      ))}
+    <div>
+      <SearchBar></SearchBar>
+      <div className="properties-container">
+        {data.map((property) => (
+          <div key={property._id} className="property-item property-card">
+            <AiFillHeart
+              size={24}
+              color={likedProperties.includes(property._id) ? '#fa3e5f' : 'rgb(192,192,194)'}
+              onClick={() => handleLike(property._id)}
+              className='like-container'
+            />
+            <img src={property.image} alt={property.name} className="img" />
+            <div className="attributes">
+              <div className="propTitle">{property.title}</div>
+              <div className="price">{property.price} MATIC</div>
+              <div className="address">{property.address}, {property.city}, {property.country}</div>
+            </div>
+            <button className="viewButton">View Property</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
