825d533a2fa7c25cd71642ba9e0fd637c04be727
sreenidhi <sarojasreenidhi@gmail.com>
2023-12-21 15:35:18 +0530

    popup for properties description

--- client/src/components/Propertylist.js
+++ client/src/components/Propertylist.js
@@ -5,6 +5,15 @@ import SearchBar from './SearchBar';
 import { AiFillHeart } from 'react-icons/ai';
 
 const Propertylist = () => {
+  const [selectedProperty, setSelectedProperty] = useState(null);
+
+  const openPopup = (property) => {
+    setSelectedProperty(property);
+  };
+
+  const closePopup = () => {
+    setSelectedProperty(null);
+  };
   const [likedProperties, setLikedProperties] = useState([]);
 
   const handleLike = (propertyId) => {
@@ -29,20 +38,145 @@ const Propertylist = () => {
               size={24}
               color={likedProperties.includes(property._id) ? '#fa3e5f' : 'rgb(192,192,194)'}
               onClick={() => handleLike(property._id)}
-              className='like-container'
-            />
+              className='like-container' />
             <img src={property.image} alt={property.name} className="img" />
             <div className="attributes">
               <div className="propTitle">{property.title}</div>
               <div className="price">{property.price} MATIC</div>
               <div className="address">{property.address}, {property.city}, {property.country}</div>
             </div>
-            <button className="viewButton">View Property</button>
+            
+            <button className="viewButton" onClick={() => openPopup(property)}>
+              View Property
+            </button>
           </div>
         ))}
+
+        {selectedProperty && (
+          <div className="popup-container">
+            <div className="popup-body">
+              <div className='popup-header'>
+                <div className='popup-content'>
+                  <h2>{selectedProperty.title}</h2> <br />
+                  <p>{selectedProperty.description}</p> <br/>
+                  <p>Price: {selectedProperty.price} MATIC</p> <br/>
+                  <p>Address: {selectedProperty.address}, {selectedProperty.city}, {selectedProperty.country}</p> <br/>
+                  <p> Facilities: Bathrooms: {selectedProperty.facilities.bathrooms}, 
+                                  Parking: {selectedProperty.facilities.parking}, 
+                                  Bedrooms: {selectedProperty.facilities.bedrooms}
+                  </p>
+                </div>
+
+                <div className='popupImg'>
+                  <img src={selectedProperty.image} alt={selectedProperty.name} width={200} height={200}></img>
+                </div>
+
+                {/* Add other property details as needed */}
+                <div className='popup-buttons'>
+                  <button className='BuyPropBtn' > Buy Property</button>
+                  <button className='closeButton' onClick={closePopup}>Close</button>
+                </div>  
+              </div>
+            </div>   
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
 export default Propertylist;
+
+
+
+
+// import React, { useState } from 'react';
+// import data from '../data.json';
+// import './Propertylist.css'; 
+// import SearchBar from './SearchBar';
+// import { AiFillHeart } from 'react-icons/ai';
+  
+
+// const Propertylist = () => {
+//   const [selectedProperty, setSelectedProperty] = useState(null);
+
+//   const openPopup = (property) => {
+//     setSelectedProperty(property);
+//   };
+
+//   const closePopup = () => {
+//     setSelectedProperty(null);
+//   };
+//   const [likedProperties, setLikedProperties] = useState([]);
+
+//   const handleLike = (propertyId) => {
+//     setLikedProperties((prevLikedProperties) => {
+//       if (prevLikedProperties.includes(propertyId)) {
+//         // If property is already liked, remove it from the liked properties
+//         return prevLikedProperties.filter((id) => id !== propertyId);
+//       } else {
+//         // If property is not liked, add it to the liked properties
+//         return [...prevLikedProperties, propertyId];
+//       }
+//     });
+//   };
+
+//   return (
+//     <div>
+//       <SearchBar></SearchBar>
+//       <div className="properties-container">
+//         {data.map((property) => (
+//           <div key={property._id} className="property-item property-card">
+//             <AiFillHeart
+//               size={24}
+//               color={likedProperties.includes(property._id) ? '#fa3e5f' : 'rgb(192,192,194)'}
+//               onClick={() => handleLike(property._id)}
+//               className='like-container' />
+//             <img src={property.image} alt={property.name} className="img" />
+//             <div className="attributes">
+//               <div className="propTitle">{property.title}</div>
+//               <div className="price">{property.price} MATIC</div>
+//               <div className="address">{property.address}, {property.city}, {property.country}</div>
+//             </div>
+            
+//           <button className="viewButton" onClick={() => openPopup(property)}>
+//             View Property
+//           </button>
+//         </div>
+//       ))}
+
+//       {selectedProperty && (
+//         <div className="popup-container">
+//           <div className="popup-body">
+//             <div className='popup-header'>
+//             <div className='popup-content'>
+//             <h2>{selectedProperty.title}</h2> <br />
+//             <p>{selectedProperty.description}</p> <br/>
+//             <p>Price: {selectedProperty.price} MATIC</p> <br/>
+
+//             <p>Address: {selectedProperty.address}, {selectedProperty.city}, {selectedProperty.country}</p> < br/>
+//             <p> Facilities: Bathrooms: {selectedProperty.facilities.bathrooms}, 
+//                             Parking: {selectedProperty.facilities.parking}, 
+//                             Bedrooms: {selectedProperty.facilities.bedrooms}
+//             </p>
+//             </div>
+
+//             <div className='popupImg'>
+//             <img src={selectedProperty.image} alt={selectedProperty.name} width={200} height={200}></img>
+//             </div>
+
+//             {/* Add other property details as needed */}
+//             <div className='popup-buttons'>
+//               <button className='BuyPropBtn' > Buy Property</button>
+//               <button className='closeButton' onClick={closePopup}>Close</button>
+//             </div>  
+//             </div>
+//           </div>   
+//       </div>
+//     )}
+//     </div>
+//   );
+// };
+
+
+// export default Propertylist;
