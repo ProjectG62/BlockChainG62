fa527178d8a0c55aa603141d34ea7d51dc96f5ac
kasi0403 <keerthikasi21@gmail.com>
2023-12-21 14:56:28 +0530

    modified the home page and added about us

---
+++ client/src/components/About.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./About.css";
+function About() {
+  return (
+    <div className="r-wrapper">
+      <div className="paddings innerWidth r-container">
+        <div className="flexColStart menu">
+          <div className="menu-left">
+            <h1 className="title">ABOUT US</h1>
+            <p className="content">
+              Welcome to CryptoEstate! where innovation meets tradition, <br />
+              and real estate transactions are redefined for the digital age.
+              <br />
+              Our Platform empowers users to seamlessly Buy, Sell,
+              <br />
+              and Lease properties, revolutionizing the world of Real Estate
+              <br /> using the secure and transparent blockchain network.
+              <br />
+              Whether you're in search of your dream home, a prime
+              <br />
+              commercial space, or an investment opportunity, <br />
+              CryptoEstate offers an array of property categories to cater
+              <br /> to your unique needs.
+            </p>
+          </div>
+          <div className=" menu-right">
+            <img src="../bc.png" width={500} height={350} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -3,22 +3,24 @@ import "./Hero.css";
 import SearchBar from "./SearchBar";
 const Hero = () => {
   return (
-    <div className="background-container">
-      <h1 className=" flexCenter text-on-img">
-        Find the <br />
-        Perfect Property,
-        <br />
-        Anywhere.
-      </h1>
-      <p className="flexCenter text-on-img-mini">
-        revolutionizing the world of Real Estate
-        <br />
-        using the secure and transparent blockchain network.
-      </p>
-      <div className="search-box">
-        <SearchBar />
+    <>
+      <div className="background-container">
+        <h1 className=" flexCenter text-on-img">
+          Find the <br />
+          Perfect Property,
+          <br />
+          Anywhere.
+        </h1>
+        <p className="flexCenter text-on-img-mini">
+          revolutionizing the world of Real Estate
+          <br />
+          using the secure and transparent blockchain network.
+        </p>
+        <div className="search-box">
+          <SearchBar />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
