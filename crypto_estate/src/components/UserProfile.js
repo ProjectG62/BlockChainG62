1cb3617a8a0a64585e2ed12c35595079b1fcedc7
rishika <rishi328g@gmail.com>
2024-01-12 00:08:19 +0530

    added username, profile-icon and metamask hash in profile page.

--- crypto_estate/src/components/UserProfile.js
+++ crypto_estate/src/components/UserProfile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{ useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import UserSideBar from "./UserSideBar";
 import WishList from "./WishList";
@@ -8,12 +8,36 @@ import BoughtProperty from "./BoughtProperty";
 import Update from "./Update";
 
 const UserProfile = () => {
-    const {activepage} = useParams()    
+    const {activepage} = useParams()  
+    const [address, setAddress] = useState('');  
+    useEffect(() => {
+      const fetchWalletAddress = async () => {
+        try {
+          const accounts = await window.ethereum.request({
+            method: 'eth_requestAccounts',
+          });
+          setAddress(accounts[0]);
+        } catch (error) {
+          console.error('Error connecting to wallet:', error.message);
+        }
+      };
+  
+      fetchWalletAddress();
+    }, []);
     return (
-      <div className="userprofile">
-
-        {/* UserProfile , showing {activepage} */}
-        <div className = 'userprofilein'>
+      <div style={{backgroundColor:"rgb(246, 244, 244)",paddingBottom:"8%"}}>
+        <div className="header">
+                    <div className="profile-info">
+                        <span className="user-name">Username</span>
+                        <div className="icon-address">
+                        <img src={"https://www.pngarts.com/files/10/Default-Profile-Picture-Download-PNG-Image.png"} alt="Profile Icon" className="profile-icon" />
+                        <span className="user-address">{address.substring(0,6 )}...{address.substring(38)}</span>
+                        </div>
+                    </div>
+                </div>
+        <div className="userprofile">  
+          {/* UserProfile , showing {activepage} */}
+          <div className = 'userprofilein'>      
           <div className="left">
             <UserSideBar activepage={activepage}/>
           </div>
@@ -25,6 +49,7 @@ const UserProfile = () => {
           </div>
         </div>
 
+        </div>
         </div>
         
     );
